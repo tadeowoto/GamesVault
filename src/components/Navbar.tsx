95e52524fa3b56606758041bb2374b0aa4f0268b
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-04-23 11:32:55 -0300

    media querys styles fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <header className="w-11/12">
+    <header className="w-11/12 md:w-6/10">
       <nav className="w-full h-8 bg-bg-card flex items-center justify-center rounded-xl">
         <ul className="flex gap-3">
           <Link to="/List">List</Link>
